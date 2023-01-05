b7dc92350d5487caf68cf067507d74243565210f
piqahamir <73425549+piqahamir@users.noreply.github.com>
2023-01-06 03:54:21 +0800

    Add files via upload

---
+++ lib/loginuser.dart
@@ -0,0 +1,277 @@
+import 'dart:convert';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:ndialog/ndialog.dart';
+import 'config.dart';
+import 'newhomestayscreen.dart';
+import 'user.dart';
+import 'homestay.dart';
+import 'package:http/http.dart' as http;
+
+class LoginUser extends StatefulWidget {
+  final User user;
+  const LoginUser({super.key, required this.user});
+
+  @override
+  State<LoginUser> createState() => _LoginUserState();
+}
+
+class _LoginUserState extends State<LoginUser> {
+  var _lat, _lng;
+  late Position _position;
+  List<Homestay> homestayList = <Homestay>[];
+  String titlecenter = "Loading...";
+  var placemarks;
+  late double screenHeight, screenWidth, resWidth;
+  int rowcount = 2;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHomestays();
+  }
+
+  @override
+  void dispose() {
+    homestayList = [];
+    print("dispose");
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    if (screenWidth <= 600) {
+      resWidth = screenWidth;
+      rowcount = 2;
+    } else {
+      resWidth = screenWidth * 0.75;
+      rowcount = 3;
+    }
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login Page"), actions: [
+        PopupMenuButton(
+            // add icon, by default "3 dot" icon
+            // icon: Icon(Icons.book)
+            itemBuilder: (context) {
+          return [
+            const PopupMenuItem<int>(
+              value: 0,
+              child: Text("New Homestay"),
+            ),
+            const PopupMenuItem<int>(
+              value: 1,
+              child: Text("Logout"),
+            ),
+          ];
+        }, onSelected: (value) {
+          if (value == 0) {
+            _gotoNewHomestay();
+            print("My account menu is selected.");
+          } else if (value == 1) {
+            print("Logout menu is selected.");
+          }
+        }),
+      ]),
+      body: homestayList.isEmpty
+          ? Center(
+              child: Text(titlecenter,
+                  style: const TextStyle(
+                      fontSize: 22, fontWeight: FontWeight.bold)))
+          : Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Your current homestay/services (${homestayList.length} found)",
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: rowcount,
+                    children: List.generate(homestayList.length, (index) {
+                      return Card(
+                        elevation: 8,
+                        child: InkWell(
+                          // onTap: () {
+                          // _showDetails(index);
+                          // },
+
+                          child: Column(children: [
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Flexible(
+                              flex: 6,
+                              child: CachedNetworkImage(
+                                width: resWidth / 2,
+                                fit: BoxFit.cover,
+                                imageUrl:
+                                    "${Config.server}/images/homestayimages/${homestayList[index].homestayId}_1.png",
+                                placeholder: (context, url) =>
+                                    const LinearProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                              ),
+                            ),
+                            Flexible(
+                                flex: 4,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        truncateString(
+                                            homestayList[index]
+                                                .homestayName
+                                                .toString(),
+                                            15),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                          "RM ${double.parse(homestayList[index].homestayPrice.toString()).toStringAsFixed(2)}"),
+                                    ],
+                                  ),
+                                ))
+                          ]),
+                        ),
+                      );
+                    }),
+                  ),
+                )
+              ],
+            ),
+    );
+  }
+
+  String truncateString(String str, int size) {
+    if (str.length > size) {
+      str = str.substring(0, size);
+      return "$str...";
+    } else {
+      return str;
+    }
+  }
+
+  Future<void> _gotoNewHomestay() async {
+    ProgressDialog progressDialog = ProgressDialog(
+      context,
+      blur: 10,
+      message: const Text("Searching your current location"),
+      title: null,
+    );
+    progressDialog.show();
+    if (await _checkPermissionGetLoc()) {
+      progressDialog.dismiss();
+      await Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (content) => NewHomestayScreen(
+                  position: _position,
+                  user: widget.user,
+                  placemarks: placemarks)));
+      _loadHomestays();
+    } else {
+      Fluttertoast.showToast(
+          msg: "Please allow the app to access the location",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          fontSize: 14.0);
+    }
+  }
+
+  Future<bool> _checkPermissionGetLoc() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        Fluttertoast.showToast(
+            msg: "Please allow the app to access the location",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            fontSize: 14.0);
+        Geolocator.openLocationSettings();
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      Fluttertoast.showToast(
+          msg: "Please allow the app to access the location",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          fontSize: 14.0);
+      Geolocator.openLocationSettings();
+      return false;
+    }
+    _position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best);
+    try {
+      placemarks = await placemarkFromCoordinates(
+          _position.latitude, _position.longitude);
+    } catch (e) {
+      Fluttertoast.showToast(
+          msg:
+              "Error in fixing your location. Make sure internet connection is available and try again.",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          fontSize: 14.0);
+      return false;
+    }
+    return true;
+  }
+
+  void _loadHomestays() {
+    http
+        .get(
+      Uri.parse(
+          "${Config.server}/php/loadhomestays.php?userid=${widget.user.id}"),
+    )
+        .then((response) {
+      if (response.statusCode == 200) {
+        var jsondata = jsonDecode(response.body);
+        if (jsondata['status'] == 'success') {
+          var extractdata = jsondata['data'];
+          if (extractdata['homestays'] != null) {
+            homestayList = <Homestay>[];
+            extractdata['homestays'].forEach((v) {
+              homestayList.add(Homestay.fromJson(v));
+            });
+            titlecenter = "Found";
+          } else {
+            titlecenter = "No homestay Available";
+            homestayList.clear();
+          }
+        } else {
+          titlecenter = "No Homestay Available";
+        }
+      } else {
+        titlecenter = "No Homestay Available";
+        homestayList.clear();
+      }
+      setState(() {});
+    });
+  }
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\piqah_vv\\flutter\\src" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\onedrive\\Documents\\fluttera221\\homestay_raya" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\piqah_vv\\flutter\\src"
+  "PROJECT_DIR=C:\\onedrive\\Documents\\fluttera221\\homestay_raya"
+  "FLUTTER_ROOT=C:\\Users\\piqah_vv\\flutter\\src"
+  "FLUTTER_EPHEMERAL_DIR=C:\\onedrive\\Documents\\fluttera221\\homestay_raya\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\onedrive\\Documents\\fluttera221\\homestay_raya"
+  "FLUTTER_TARGET=C:\\onedrive\\Documents\\fluttera221\\homestay_raya\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\onedrive\\Documents\\fluttera221\\homestay_raya\\.dart_tool\\package_config.json"
+)
